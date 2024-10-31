d7843274c4e7121fabfa48210d20d02679a681a2
devAlex.ᴱᵀᴴ <88956822+devAlexDotEth@users.noreply.github.com>
2024-10-31 12:17:43 -0400

    fixed images

--- src/data/zombieImages.ts
+++ src/data/zombieImages.ts
@@ -5,14 +5,14 @@ export interface ZombieNFT {
   }
   
   export const zombieImages: ZombieNFT[] = [
-    { id: 0, name: "ZBK #0", image: "./src/assets/20.png" },
-    { id: 1, name: "ZBK #1", image: "./src/assets/21.png" },
-    { id: 2, name: "ZBK #2", image: "./src/assets/22.png" },
-    { id: 3, name: "ZBK #3", image: "./src/assets/23.png" },
-    { id: 4, name: "ZBK #4", image: "./src/assets/24.png" },
-    { id: 5, name: "ZBK #5", image: "./src/assets/25.png" },
-    { id: 6, name: "ZBK #6", image: "./src/assets/26.png" },
-    { id: 7, name: "ZBK #7", image: "./src/assets/27.png" },
-    { id: 8, name: "ZBK #8", image: "./src/assets/28.png" },
-    { id: 9, name: "ZBK #9", image: "./src/assets/29.png" },
+    { id: 0, name: "ZBK #0", image: "/assets/20.png" },
+    { id: 1, name: "ZBK #1", image: "/assets/21.png" },
+    { id: 2, name: "ZBK #2", image: "/assets/22.png" },
+    { id: 3, name: "ZBK #3", image: "/assets/23.png" },
+    { id: 4, name: "ZBK #4", image: "/assets/24.png" },
+    { id: 5, name: "ZBK #5", image: "/assets/25.png" },
+    { id: 6, name: "ZBK #6", image: "/assets/26.png" },
+    { id: 7, name: "ZBK #7", image: "/assets/27.png" },
+    { id: 8, name: "ZBK #8", image: "/assets/28.png" },
+    { id: 9, name: "ZBK #9", image: "/assets/29.png" },
   ];
